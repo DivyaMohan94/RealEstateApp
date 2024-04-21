aa08d9373e560c4d4f04c055d6bebe6a52ee3b26
DivyaMohan <divyamohan94@gmail.com>
2024-04-21 02:54:49 -0700

    Added code for pagination and refactored code

--- real_estate_fe/src/components/SearchResult.jsx
+++ real_estate_fe/src/components/SearchResult.jsx
@@ -1,122 +1,64 @@
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-  Button,
-} from "@material-tailwind/react";
+import { Typography } from "@material-tailwind/react";
 
 import data from "../assets/data.json";
 import React, { useState } from "react";
 import HomeCard from "./HomeCard";
+import Paginate from "./Paginate";
+import "../Styles/Searchbar.css";
 
 export default function SearchResult() {
-//   const [showModal, setShowModal] = useState(false);
-//   const [selectedHome, setSelectedHome] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(4);
 
-//   const closeModal = (e) => {
-//     if (e.target.id === "backdrop") {
-//       setShowModal(false);
-//     }
-//   };
+  let indexOfLastPost = currentPage * postsPerPage;
+  let indexOfFirstPost = indexOfLastPost - postsPerPage;
+  let currentPosts = data.homes.slice(indexOfFirstPost, indexOfLastPost);
 
-//   const cards = data.homes.map((home) => {
-//     return (
-//         <>
-//       <Card className="mt-6 w-96" id={home.id}>
-//         <CardHeader color="blue-gray" className="relative h-56">
-//           <img
-//             src="https://photos.zillowstatic.com/fp/14dcd6eae27f434426ba98c116cb7081-uncropped_scaled_within_1536_1152.webp"
-//             alt="card-image"
-//           />
-//         </CardHeader>
-//         <CardBody>
-//           <Typography variant="h5" color="blue-gray" className="mb-2">
-//             {home.address}
-//           </Typography>
-//           <div style={{ display: "flex", alignItems: "center" }}>
-//             <Typography variant="h6">
-//               {home.price} | {home.beds} Beds | {home.bath} Baths | {home.sqft}{" "}
-//               sqft
-//             </Typography>
-//           </div>
-//         </CardBody>
-//         <CardFooter className="pt-0">
-//           <Button
-//             id={home.id}
-//             onClick={(e) => {
-//                 console.log(cards[e.target.id].address);
-//               setShowModal(true);
-//               setSelectedHome(e.target.id);
-//             }}
-//           >
-//             Read More
-//           </Button>
-//         </CardFooter>
-//       </Card>
-//       </>
-//     );
-//   });
+  const paginate = (selected) => {
+    setCurrentPage(selected);
+    pageData();
+  };
+  const previousPage = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+      pageData();
+    }
+  };
+
+  const nextPage = () => {
+    if (currentPage !== Math.ceil(data.homes.length / postsPerPage)) {
+      setCurrentPage(currentPage + 1);
+      pageData();
+    }
+  };
+
+  const pageData = () => {
+    indexOfLastPost = currentPage * postsPerPage;
+    indexOfFirstPost = indexOfLastPost - postsPerPage;
+    currentPosts = data.homes.slice(indexOfFirstPost, indexOfLastPost);
+    console.log(currentPosts);
+  };
 
   return (
     <div>
       <Typography variant="h3">Browse homes</Typography>
       <div className="max-w-[1600px] mx-auto grid md:grid-cols-4 gap-8">
-        {data.homes.map((home) => {
-            return <HomeCard home = {home}/>
-        })}
+        {currentPosts ? (
+          currentPosts.map((home) => {
+            return <HomeCard home={home} id={home.id} />;
+          })
+        ) : (
+          <div>No data...</div>
+        )}
       </div>
-      
+      <Paginate
+        postsPerPage={postsPerPage}
+        totalPosts={data.homes.length}
+        currentPage={currentPage}
+        paginate={paginate}
+        previousPage={previousPage}
+        nextPage={nextPage}
+      />
     </div>
   );
 }
-
-
-
-// {showModal ? (
-//     <>
-//       <div
-//         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-//         onClick={closeModal}
-//         id="backdrop"
-//       >
-//         <div className="relative w-auto my-6 mx-auto max-w-4xl">
-//           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-//             <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-//               <h3 className="text-3xl font-semibold">{
-//               home.address}</h3>
-//               <button
-//                 className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-//                 onClick={() => setShowModal(false)}
-//               >
-//                 <span className="bg-transparent text-black opacity-700 h-6 w-6 text-2xl block outline-none focus:outline-none">
-//                   ×
-//                 </span>
-//               </button>
-//             </div>
-//             <img
-//               alt="nature"
-//               className="h-[24rem] w-full rounded-lg object-cover object-center p-4"
-//               src={home.address}
-//             />
-//             <div className="relative p-6 flex-auto">
-//               <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-//                 {home.description}
-//               </p>
-//             </div>
-//             <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-//               <button
-//                 className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-//                 type="button"
-//                 onClick={() => setShowModal(false)}
-//               >
-//                 Close
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-//     </>
-//   ) : null}
--- real_estate_fe/src/components/Searchbar.jsx
+++ real_estate_fe/src/components/Searchbar.jsx
@@ -33,8 +33,9 @@ export default function Searchbar() {
           <input
             type="search"
             id="search"
-            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+            className="block w-full p-4 ps-10 text-md text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
             placeholder="Search for City, Zip, Neighborhood, Features..."
+            style = {{width:"500px"}}
           />
 
           <button
