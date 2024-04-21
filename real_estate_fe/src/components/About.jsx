b7611db3432a545985573c7be3a8ef273121a5e2
DivyaMohan <divyamohan94@gmail.com>
2024-04-21 03:37:38 -0700

    Added sample content for login and about page

--- real_estate_fe/src/components/About.jsx
+++ real_estate_fe/src/components/About.jsx
@@ -1,7 +1,40 @@
-export default function About(){
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Typography,
+    Button,
+  } from "@material-tailwind/react";
+
+  import "../Styles/Login.css"
+   
+  export default function About() {
     return (
-        <h4>
-            About
-        </h4>
-    )
-}
+        <div className = "card">
+      <Card className="w-full max-w-[120 rem] flex-row" >
+        <CardHeader
+          shadow={false}
+          floated={false}
+          className="m-0 w-2/5 shrink-0 rounded-r-none"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            alt="card-image"
+            className="h-full w-full object-cover"
+          />
+        </CardHeader>
+        <CardBody>
+          <Typography variant="h6" color="gray" className="mb-4 uppercase">
+            HomeGuideAI Startup
+          </Typography>
+          <Typography variant="h4" color="blue-gray" className="mb-2">
+            Launching AI based Real Estate App this month!
+          </Typography>
+          <Typography color="gray" className="mb-8 font-normal" style={{textAlign:"justify"}}>
+          At HomeGuideAI, we empower home buyers and sellers with innovative AI-driven solutions to streamline the real estate process. Our advanced search technology allows you to find homes that perfectly match your desired features and lifestyle. With our app, you can monitor bids and receive AI-generated insights to guide you towards the best possible offer. We simplify the entire journey, from initial search to closing the deal, by eliminating time-consuming documentation review and providing seamless, efficient transactions. Discover the future of real estate with HomeGuideAI-your trusted partner for smarter home buying and selling.
+          </Typography>
+        </CardBody>
+      </Card>
+      </div>
+    );
+  }
--- real_estate_fe/src/components/Login.jsx
+++ real_estate_fe/src/components/Login.jsx
@@ -1,7 +1,54 @@
+import {
+    Card,
+    Input,
+    Button,
+    Typography,
+  } from "@material-tailwind/react";
+
+  import React, {useState} from "react";
+  import "../Styles/Login.css"
+
 export default function Login(){
+    const [email, setEmail] = useState("");
+    const [passowrd, setPassword] = useState("");
     return (
-        <h4>
-            Login
-        </h4>
-    )
+        <Card color="transparent" shadow={false} className = "card">
+         <Typography variant="h4" color="blue-gray">
+           Login
+         </Typography>
+         <Typography color="gray" className="mt-1 font-normal">
+           Nice to meet you! Enter your details to login.
+         </Typography>
+         <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" style = {{alignSelf:"center"}}>
+           <div className="mb-1 flex flex-col gap-6">
+             <Typography variant="h6" color="blue-gray" className="-mb-3">
+               Your Email
+             </Typography>
+             <Input
+               size="lg"
+               placeholder="name@mail.com"
+               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+               labelProps={{
+                 className: "before:content-none after:content-none",
+               }}
+             />
+             <Typography variant="h6" color="blue-gray" className="-mb-3">
+               Password
+             </Typography>
+             <Input
+               type="password"
+               size="lg"
+               placeholder="********"
+               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+               labelProps={{
+                 className: "before:content-none after:content-none",
+               }}
+             />
+           </div>
+           <Button className="mt-6" fullWidth>
+             Login
+           </Button>
+         </form>
+       </Card>
+     )
 }
